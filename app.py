4c1473e7679a53e8fa3d7ec4cf71ce8ab80bd311
Daveletterm <101953693+Daveletterm@users.noreply.github.com>
2025-11-16 15:30:27 -0500

    Fix forward reference for Upload annotations

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from flask import Flask, render_template, request, redirect, url_for, session, flash, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
